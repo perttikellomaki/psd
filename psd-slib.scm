965fdb7fc9cb0025c1f3a87435d21c832c085bb6
Pertti Kellomaki <pertti.kellomaki@iki.fi>
2022-08-26 15:59:57 +0300

    Remove references to GPL in the source files.

--- psd-slib.scm
+++ psd-slib.scm
@@ -4,20 +4,6 @@
 ;;;; psd -- a portable Scheme debugger, version 1.1
 ;;;; Copyright (C) 1992 Pertti Kellomaki, pk@cs.tut.fi
 
-;;;; This program is free software; you can redistribute it and/or modify
-;;;; it under the terms of the GNU General Public License as published by
-;;;; the Free Software Foundation; either version 1, or (at your option)
-;;;; any later version.
-
-;;;; This program is distributed in the hope that it will be useful,
-;;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-;;;; GNU General Public License for more details.
-
-;;;; You should have received a copy of the GNU General Public License
-;;;; along with this program; if not, write to the Free Software
-;;;; Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
-;;;; See file COPYING in the psd distribution.
 ;;;;
 ;;;;  $Log: psd-slib.scm,v $
 ;;;; Revision 1.1  1993/10/04  14:05:23  pk
--- psd.el
+++ psd.el
@@ -8,20 +8,6 @@
 ;;; Run psd under GNU Emacs 18.58
 ;;; Copyright (C) 1992 Pertti Kellom\"aki.
 
-;;; Psd is free software; you can redistribute it and/or modify
-;;; it under the terms of the GNU General Public License as published by
-;;; the Free Software Foundation; either version 1, or (at your option)
-;;; any later version.
-
-;;; Psd is distributed in the hope that it will be useful,
-;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;;; GNU General Public License for more details.
-
-;;; You should have received a copy of the GNU General Public License
-;;; along with psd; see the file COPYING.  If not, write to
-;;; the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
-
 ;;; Author: Pertti Kellom\"aki, Tampere University of Technology, Finland
 ;;;    pk@cs.tut.fi
 
